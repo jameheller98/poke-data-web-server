71e09030bc1d6d96b74d4ac626786d0965a71201
jameheller98 <1651012211tuan@ou.edu.vn>
2021-11-06 23:14:49 +0700

    fix error h10 2

--- src/main/java/com/ou/pokemondata/config/DatabaseConfig.java
+++ src/main/java/com/ou/pokemondata/config/DatabaseConfig.java
@@ -3,13 +3,10 @@ package com.ou.pokemondata.config;
 
 import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DatabaseConfig {
 
  @Value("${spring.datasource.url}")
